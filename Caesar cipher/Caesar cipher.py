7617de9df5e8d7a783a1c94b1ef44fa8fbed1e8a
Jessie-jushizhe <jessiejushizhe@gmail.com>
2024-06-30 14:26:07 +0800

    应该是成了（反正自测没啥毛病

---
+++ Caesar cipher/Caesar cipher.py
@@ -0,0 +1,43 @@
+def encode(sentence, key):
+    new_sentence=""
+    for l in sentence:
+        if l==" ":
+            new_sentence+=" "
+
+        elif 1<=key<=26:
+            new_a=ord(l)+key
+            if new_a>ord('z'):
+                new_sentence+=chr(new_a-26)
+
+            else:
+                new_sentence+=chr(new_a)
+
+    return new_sentence
+
+def decode(sentence, key):
+    new_sentence = ""
+    for l in sentence:
+        if l == " ":
+            new_sentence += " "
+
+        elif 1 <= key <= 26:
+            new_a = ord(l) - key
+            if new_a < ord('a'):
+                new_sentence += chr(new_a + 26)
+
+            else:
+                new_sentence += chr(new_a)
+
+    return new_sentence
+
+d_or_e=input("Do you want to encode or decode your sentence? (d/e) :")
+if d_or_e == "d":
+    sentence=input("Enter your sentence: ")
+    key=int(input("Enter your key: "))
+    print(decode(sentence, key))
+
+if d_or_e == "e":
+    sentence=input("Enter your sentence: ")
+    key=int(input("Enter your key: "))
+    print(encode(sentence, key))
+
