0ceec8ccd44146c26a232da9eb1b24ef77509717
Jessie-jushizhe <jessiejushizhe@gmail.com>
2024-07-01 13:26:20 +0800

    漏洞修复完成版

--- Caesar cipher/Caesar cipher.py
+++ Caesar cipher/Caesar cipher.py
@@ -6,12 +6,12 @@ def encode(sentence, key):
 
         elif 1 <= key <= 26:
             new_a = ord(letter)+key
-            if ord('a') <= new_a <= ord('z'):
+            if ord('a') <= ord(letter) <= ord('z'):
                 if new_a > ord('z'):
                     new_sentence += chr(new_a-26)
                 else:
                     new_sentence += chr(new_a)
-            elif ord('A') <= new_a <= ord('Z'):
+            elif ord('A') <= ord(letter) <= ord('Z'):
                 if new_a > ord('Z'):
                     new_sentence += chr(new_a - 26)
                 else:
@@ -27,12 +27,12 @@ def decode(sentence, key):
 
         elif 1 <= key <= 26:
             new_a = ord(letter) - key
-            if ord('a') <= new_a <= ord('z'):
+            if ord('a') <= ord(letter) <= ord('z'):
                 if new_a < ord('a'):
                     new_sentence += chr(new_a + 26)
                 else:
                     new_sentence += chr(new_a)
-            elif ord('A') <= new_a <= ord('Z'):
+            elif ord('A') <= ord(letter) <= ord('Z'):
                 if new_a < ord('A'):
                     new_sentence += chr(new_a + 26)
 
@@ -44,12 +44,15 @@ def decode(sentence, key):
 d_or_e = input("Do you want to encode or decode your sentence? (d/e) :")
 while d_or_e != "d" and d_or_e != "e":
     d_or_e = input("Do you want to encode or decode your sentence? (d/e) :")
-if d_or_e == "d":
+sentence = input("Enter your sentence: ")
+while sentence.isalpha() != True:
     sentence = input("Enter your sentence: ")
+
+key = int(input("Enter your key: "))
+while key.isnumeric() != True:
     key = int(input("Enter your key: "))
+
+if d_or_e == "d":
     print(decode(sentence, key))
 elif d_or_e == "e":
-    sentence = input("Enter your sentence: ")
-    key = int(input("Enter your key: "))
     print(encode(sentence, key))
-
